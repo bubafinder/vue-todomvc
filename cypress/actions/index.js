fba9dfe2cd738cd8a0546a3b216f6bc9af13b60f
Marko Lovric <lovric.split@gmail.com>
2023-05-26 14:48:21 +0200

    Const to functions

--- cypress/actions/index.js
+++ cypress/actions/index.js
@@ -1,31 +1,42 @@
-export const visit = () => {
+function visit() {
   cy.visit('/')
 }
 
-export const resetDB = (todos = []) => {
+function resetDB(todos = []) {
   cy.request('POST', '/reset', { todos })
 }
 
-export const getTodoItem = (index = 1) => {
+function getTodoItem(index = 1) {
   return cy.get(`.todo-list li:nth-child(${index})`)
 }
 
-export const getTodoApp = () => {
+function getTodoApp() {
   return cy.get('.todoapp')
 }
 
-export const addTodoItem = (text = 'Item') => {
+function addTodoItem(text = 'Item') {
   return cy.get('.new-todo').type(`${text}{enter}`)
 }
 
-export const deleteTodoItem = (index = 1) => {
+function deleteTodoItem(index = 1) {
   return getTodoItem(index).find('.destroy').click({ force: true })
 }
 
-export const completeTodoItem = (index = 1) => {
+function completeTodoItem(index = 1) {
   return getTodoItem(index).find('.toggle').check()
 }
 
-export const uncompleteTodoItem = (index = 1) => {
+function uncompleteTodoItem(index = 1) {
   return getTodoItem(index).find('.toggle').check()
+}
+
+module.exports = {
+  visit,
+  resetDB,
+  getTodoApp,
+  getTodoItem,
+  addTodoItem,
+  deleteTodoItem,
+  completeTodoItem,
+  uncompleteTodoItem
 }
