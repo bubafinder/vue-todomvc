c77ef4fb007a42a19425a47efbcb8acf276f9643
nikola-zitko <nzitko@extensionengine.com>
2023-05-26 14:55:25 +0200

    Prettier

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -4,30 +4,30 @@
 import * as action from '../actions'
 
 describe('DUMP workshop 2023', () => {
-beforeEach(() => {
-  action.resetDB()
-  action.visit()
-  action.getTodoApp().should('be.visible')
-})
+  beforeEach(() => {
+    action.resetDB()
+    action.visit()
+    action.getTodoApp().should('be.visible')
+  })
 
-it('Create TODO item', () => {
-  action.addTodoItem()
-})
+  it('Create TODO item', () => {
+    action.addTodoItem()
+  })
 
-it('Complete TODO item', () => {
-  action.addTodoItem()
-  action.addTodoItem('Second item')
-  action.completeTodoItem()
-})
+  it('Complete TODO item', () => {
+    action.addTodoItem()
+    action.addTodoItem('Second item')
+    action.completeTodoItem()
+  })
 
-it('Uncomplete TODO item', () => {
-  action.addTodoItem()
-  action.completeTodoItem()
-  action.uncompleteTodoItem()
-})
+  it('Uncomplete TODO item', () => {
+    action.addTodoItem()
+    action.completeTodoItem()
+    action.uncompleteTodoItem()
+  })
 
-it('Delete TODO item', () => {
-  action.addTodoItem()
-  action.deleteTodoItem()
-})
+  it('Delete TODO item', () => {
+    action.addTodoItem()
+    action.deleteTodoItem()
+  })
 })
